18981e2cc1ff56696d8a153a7789155e4d11fea3
LakshayRohilla10 <lakshay.rohilla@sms-group.com>
2023-05-03 12:37:44 +0530

    Adding complete code

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Count} from "./Components/Count";
 
 function App() {
   return (
@@ -8,16 +9,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         Counter Using React & Mobx(State Management)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Count/>
       </header>
     </div>
   );
